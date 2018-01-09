e37224ae83f75fe1bfee6cd88b4ee1e7a8059c1f
Suren Baghdasaryan <surenb@google.com>
2018-01-09 15:47:40 -0800

    sakura: Set lmkd configuration properties
    Bug: 63631020
    
    Change-Id: I16a183e8e4625e8b98cefdc9c8a7b650556a2b55
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,6 +129,9 @@ USE_OPENGL_RENDERER := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
+# Enable stats logging in LMKD
+TARGET_LMKD_STATS_LOG := true
+
 # Filesystem
 TARGET_USES_MKE2FS := true
 TARGET_USERIMAGES_USE_F2FS := true
