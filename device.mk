b1f9a6d62c137d7e4c0a1980469a8e5e882ff1a7
Zinadin Zidan <tuhinsultana075@gmail.com>
2020-03-20 20:29:37 +0600

    sakura: Update Camera From Daisy Q Release
    * Updated From daisy-user-10-QKQ1.191002.002-V11.0.2.0.QDLMIXM-release-keys
    * Droped Useless Hack Libs
    * Also Reverted 329e1ee314a1c2ec8962acf15b71c2dc91876df2 & ed0fa0c9e7bc68b0d24168534a3394455c2edb21
    - Also Thanks To @acras01

--- device.mk
+++ device.mk
@@ -118,8 +118,13 @@ BOOT_SECURITY_PATCH = $(PLATFORM_SECURITY_PATCH)
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.cameraservice.common@2.0 \
+    android.frameworks.cameraservice.device@2.0 \
+    android.frameworks.cameraservice.service@2.0 \
+    android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0
 
 # Consumerir
@@ -365,10 +370,6 @@ PRODUCT_PACKAGES += \
     vndk_package \
     libstdc++.vendor \
     libcutils.so
-
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v27/arm/arch-arm-armv7-a-neon/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.frameworks.sensorservice@1.0-v27.so \
-    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.frameworks.sensorservice@1.0-v27.so
 
 # Wallpapers
 PRODUCT_PACKAGES += \
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -10,15 +10,15 @@ set -e
 DEVICE=sakura
 VENDOR=xiaomi
 
-INITIAL_COPYRIGHT_YEAR=2019
+INITIAL_COPYRIGHT_YEAR=2020
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-MK_ROOT="${MY_DIR}/../../.."
+AOSP_ROOT="${MY_DIR}/../../.."
 
-HELPER="${MK_ROOT}/vendor/aosp/build/tools/extract_utils.sh"
+HELPER="${AOSP_ROOT}/vendor/aosp/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -26,7 +26,7 @@ fi
 source "${HELPER}"
 
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${MK_ROOT}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${AOSP_ROOT}"
 
 # Copyright headers and guards
 write_headers
